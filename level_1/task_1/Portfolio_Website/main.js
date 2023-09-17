0e930cb4db9e56ad5d6ca515b6f4c0cc442d6ff1
ShivaniKaushik@8585 <shivani10kaushik5@gmail.com>
2023-09-17 23:00:18 +0530

    home

--- level_1/task_1/Portfolio_Website/main.js
+++ level_1/task_1/Portfolio_Website/main.js
@@ -4,12 +4,14 @@ const navToogle= document.getElementById("bar");
 const navClose = document.getElementById("nav_close");
 if(navToogle){
     navToogle.addEventListener("click",()=>{
-        navMenu.classList.add("show_menu");
+        // navMenu.classList.add("show_menu");
+        console.log('click')
     })
 }
 // hiiden
 if(navClose){
     navClose.addEventListener("click",()=>{
+        console.log('click')
         navMenu.classList.remove("show_menu");
     })
 }
@@ -19,11 +21,9 @@ const navLinks= document.querySelectorAll('.link');
 console.log(navLinks)
 const linkAction =()=>{
     const navMenu = document.getElementById("nav_links");
-    console.log(navMenu);
+
     navMenu.classList.remove("show_menu");
 
 }
-const shi =() =>{
-    console.log('click')
-}
+
 navLinks.forEach(n=> n.addEventListener('click',linkAction));
