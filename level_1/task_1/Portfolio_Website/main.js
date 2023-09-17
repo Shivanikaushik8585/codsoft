a3dcf4d4bda26658f3d8957510bebe12c8190e69
ShivaniKaushik@8585 <shivani10kaushik5@gmail.com>
2023-09-17 09:01:54 +0530

    add toogler

--- level_1/task_1/Portfolio_Website/main.js
+++ level_1/task_1/Portfolio_Website/main.js
@@ -1,4 +1,5 @@
 const navMenu= document.getElementById("nav_links");
+
 const navToogle= document.getElementById("bar");
 const navClose = document.getElementById("nav_close");
 if(navToogle){
@@ -12,3 +13,17 @@ if(navClose){
         navMenu.classList.remove("show_menu");
     })
 }
+
+//  Remove menu MObile
+const navLinks= document.querySelectorAll('.link');
+console.log(navLinks)
+const linkAction =()=>{
+    const navMenu = document.getElementById("nav_links");
+    console.log(navMenu);
+    navMenu.classList.remove("show_menu");
+
+}
+const shi =() =>{
+    console.log('click')
+}
+navLinks.forEach(n=> n.addEventListener('click',linkAction));
